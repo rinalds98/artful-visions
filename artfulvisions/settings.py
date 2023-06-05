26ddda869b76907ba94acd1b4fc5f9f7349a4363
rz <rinalds98@gmail.com>
2023-06-05 10:59:55 +0100

    added stripe payment processor and checkout success template

--- artfulvisions/settings.py
+++ artfulvisions/settings.py
@@ -176,3 +176,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Stripe
+STRIPE_CURRENCY = 'usd'
+STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY', '')
