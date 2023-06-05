008a06633a92a8e9f3ddd365f314a22f7cdb4b72
rz <rinalds98@gmail.com>
2023-06-05 11:45:17 +0100

    Added webhook handlers

--- artfulvisions/settings.py
+++ artfulvisions/settings.py
@@ -182,3 +182,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY', '')
+STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
