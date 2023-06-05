5f0ccbe8028a096a47d4a9573ffb3e26a58ae3e8
rz <rinalds98@gmail.com>
2023-06-05 10:27:30 +0100

    added checkout views & templates and added crispy forms

--- checkout/views.py
+++ checkout/views.py
@@ -1,8 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
+from .forms import OrderForm
 
 
 def checkout(request):
-    """
-    A view that returns the checkout page
-    """
-    return render(request, 'home/index.html')
+    bag = request.session.get('bag', {})
+    if not bag:
+        messages.error(request, "There's nothing in your bag at the moment")
+        return redirect(reverse('products'))
+
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+    }
+
+    return render(request, template, context)
