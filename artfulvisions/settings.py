45aceccfc2f350a5fd582affd395414c608c1983
rz <rinalds98@gmail.com>
2023-06-25 12:05:31 +0100

    Ran html checker and fixed minor bugs

--- artfulvisions/settings.py
+++ artfulvisions/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['artful-visions-e583fd12b6db.herokuapp.com', 'localhost', '127.0.0.1', '4788-212-129-80-239.ngrok-free.app']
 
