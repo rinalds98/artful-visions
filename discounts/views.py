2bb64000b152880943ed96de1b78fe5064f90384
rz <rinalds98@gmail.com>
2023-06-29 13:01:10 +0100

    Debug false

--- discounts/views.py
+++ discounts/views.py
@@ -7,18 +7,13 @@ def discount_validation(request):
         discount_code = request.POST.get('discount_code')
         if discount_code:
             try:
-                discount = DiscountCode.objects.get(code=discount_code)
                 request.session['discount_code'] = discount_code
-                # Discount code is valid
                 context = {'valid': True}
             except DiscountCode.DoesNotExist:
-                # Discount code is invalid
                 context = {'valid': False}
         else:
-            # Discount code not provided
             context = {'valid': False}
     else:
-        # Invalid request method
         context = {'valid': False}
 
     return JsonResponse(context)
