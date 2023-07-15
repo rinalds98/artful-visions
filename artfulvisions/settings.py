b747d7ffc0196528592073bfc39c082b1f41595a
rz <rinalds98@gmail.com>
2023-07-15 15:33:03 +0100

    Final deployment

--- artfulvisions/settings.py
+++ artfulvisions/settings.py
@@ -29,9 +29,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['artful-visions-e583fd12b6db.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['artful-visions-e583fd12b6db.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
